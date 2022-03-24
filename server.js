e0c233024195233aa237fcf4e1b87efddf8fa781
Josh Liuaana <98368294+josh-liuaana@users.noreply.github.com>
2022-03-25 11:49:23 +1300

    update get function

--- server.js
+++ server.js
@@ -19,10 +19,14 @@ server.get('/', (req, res) => {
   res.send('<h1>Dont look now goats are getting dressed</h1>')
 })
 
-server.get('/goat/1', (req, res) => {
+server.get('/goat/:id?', (req, res) => {
+  const id = Number(req.params.id)
+  console.log(id)
   fs.readFile('./goat.json', 'utf-8', (err, data) => {
     if (err) return res.status(500).send(err.message)
-    res.render('game', data.goats)
+    const parsedData = JSON.parse(data)
+    const theGoat = parsedData.goats.find((goat) => goat.id === id)
+    res.render('game', theGoat)
   })
 })
 
