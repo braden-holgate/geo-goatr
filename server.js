6766aed340ec3e089b4fe25a55c1630be5b99757
Josh Liuaana <98368294+josh-liuaana@users.noreply.github.com>
2022-03-25 10:52:47 +1300

    initial get route

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
 const express = require('express')
 const hbs = require('handlebars')
+const goat = require('./goat.json')
+const fs = require('fs')
 
 const server = express()
+
 
 // Server configuration
 server.use(express.static('public'))
 server.use(express.urlencoded({ extended: false }))
 
 // Handlebars configuration
-// server.engine('hbs', hbs({ extname: 'hbs' }))
-// server.set('view engine', 'hbs')
+server.engine('hbs', hbs({ extname: 'hbs' }))
+server.set('view engine', 'hbs')
 
 // routers
 
 server.get('/', (req, res) => {
   res.send('<h1>Dont look now goats are getting dressed</h1>')
+})
+
+server.get('/goat/1', (req, res) => {
+  fs.readFile(./goat.json, 'utf-8', (err, data) => {
+    if (err) return res.status(500).send(err.message)
+    res.render('game', data)
+  })
 })
 
 module.exports = server
